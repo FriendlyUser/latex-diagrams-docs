a44fabdc123bd4ab21fa5a557d5668ac82b79394
David Li <davidli012345@gmail.com>
2023-02-19 22:17:28 -0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -38,7 +38,7 @@ const config: DocsThemeConfig = {
         console.log(adjustedFilePath)
         adjustedFilePath = adjustedFilePath.replace(newPrePage, "").replace("pages", `/blob/master/${newPrePage}`).replace(".mdx", ".tex")
       }
-      const fullPath = "https://github.com/FriendlyUser/LatexDiagrams" + adjustedFilePath
+      const fullPath = "https://github.com/FriendlyUser/LatexDiagrams/" + adjustedFilePath
       return (<a className={className} href={fullPath}>{children} → </a>)
     }
   }
